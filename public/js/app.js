b94a3d2b953c91aec97aff366f94e65f8a5e73c4
Steven Wilkin <stevenwilkin@gmail.com>
2010-03-19 00:49:42 +0000

    display new comments as they arrive

--- public/js/app.js
+++ public/js/app.js
@@ -29,13 +29,48 @@ var Client = (function(){
 		return false;
 	}
 
+	function display(comments){
+		for(i in comments){
+			var
+				c = comments[i],
+				ts = new Date(c.timestamp),
+				t = {
+					h: ts.getHours(),
+					m: ts.getMinutes(),
+					s: ts.getSeconds()
+				};
+			for(i in t){
+				if(t[i] < 10)
+					t[i] = '0' + t[i];
+			}
+			var
+				time = t.h + ':' + t.m + ':' + t.s,
+				li = '<li><span>' + time + '</span>' + '<p>' + c.comment + '</p>'  + '</li>';
+			$('#comments ol').append(li);
+		}
+		since = comments[comments.length - 1].timestamp;
+		// scroll new comment into view if the mouse isn't over the comments list
+		if(!$('#comments').data('mouseover'))
+			$('#comments').scrollTop($('ol').height());	
+	}
+
 	function poll(){
-		console.log('poll');
+		$.ajax({
+			url: '/comments' + ((since) ? '/' + since : ''),
+			success: display,
+			complete: poll
+		});
 	}
 
 	return {
 		init: function(){
 			$('#submit').click(addComment);
+			$('#comments').mouseover(function(){
+				$(this).data('mouseover', true);
+			});
+			$('#comments').mouseout(function(){
+				$(this).data('mouseover', false);
+			});
 			poll();
 		}
 	}
